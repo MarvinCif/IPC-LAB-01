38b630a8ef71a7b6b435da4b0172a54417e430f9
MarvinCif <144157889+MarvinCif@users.noreply.github.com>
2023-09-05 11:51:40 -0600

    Add files via upload
    Programa de operaciones básicas

---
+++ L1-MECM_1167723.py
@@ -0,0 +1,24 @@
+#Introducción a programación - Sección:17
+#05/09/2023
+#Marvin Estuardo Cifuentes Majía
+#Objetivo:Crear un programa que realice las operaciones basicas.
+#Entrada: Numero1, Numero2
+""" PROCESO
+1. Solicitar al usuario dos numeros enteros y realizar los cálculos (Suma, resta, multiplicacion y division)
+2. Mostrar los resultados.
+"""
+""" SALIDA
+3. Resultado de suma
+4. Resultado de resta
+5. Resultado de multiplicacion
+6. Resultado de division
+"""
+
+print ("Mi primer programa")
+print("Operaciones con numeros\n")
+Numero1=int(input("ingrese un numero: "))
+Numero2=int(input("ingrese un numero: "))
+print("Suma: ", Numero1+Numero2)
+print("Resta: ", Numero1-Numero2)
+print("Multiplicacion: ", Numero1*Numero2)
+print("Division: ", Numero1/Numero2)
