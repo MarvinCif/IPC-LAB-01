d02ff4549e9d1a8caa9dfda701fde85fd7a39217
MarvinCif <144157889+MarvinCif@users.noreply.github.com>
2023-09-26 16:59:24 -0600

    Update and rename LS8-MECM_1167723.py to Semana 6/LS8-MECM_1167723.py

--- Semana 8/LS8-MECM_1167723.py
+++ Semana 8/LS8-MECM_1167723.py
@@ -20,48 +20,64 @@
 import math
 import os
 while True:
-    os.system('cls')
-    print ("LS 8 - FACTORIAL Y SECUENCIA DE FIBONACCI\n")
-    print("1- Factorial\n2- Secuencia de Fibonacci\n3- Salir")
-    Numero1=int(input("Seleccione una opción: "))
     try:
-        if (Numero1 == 1):
-            os.system('cls')
-            print("OPCION FACTORIAL")
-            try:
-                Numero2=int(input("Ingrese un numero: "))
-                contador = 1
-                while contador <= Numero2:
-                    print(f"{contador}",end='')
-                    contador = contador + 1
-                    if (contador<=Numero2):
-                        print(" X ", end='')
-                    else:
-                        print(" = ", math.factorial(Numero2), "\n\n")
+        os.system('cls')
+        print ("LS 8 - FACTORIAL Y SECUENCIA DE FIBONACCI\n")
+        print("1- Factorial\n2- Secuencia de Fibonacci\n3- Salir")
+        Numero1=int(input("Seleccione una opción: "))
+        try:
+            if (Numero1 == 1):
+                os.system('cls')
+                print("OPCION FACTORIAL")
+                try:
+                    Numero2=int(input("Ingrese un numero: "))
+                    if Numero2 < 0:
+                        print("NO EXISTEN LOS FACTORIALES NEGATIVOS\n\n")
                         os.system('PAUSE')
-                else:
-                    print(math.factorial(Numero2))
-            except ValueError:
-                print("El valor ingresado no es un numero")
-        elif(Numero1 == 2):
-            os.system('cls')
-            def fibonacci(n):
-                fibseq = []
-                a, b = 0, 1
-                for _ in range(n):
-                    fibseq.append(a)
-                    a, b = b, a + b
-                print(fibseq) 
-            try:
-                n = int(input("Ingrese un numero para la secuencia de Fibonacci: "))
-                secuencia = fibonacci(n)
-            except ValueError:
-                print("El valor ingresado no es un numero")
-            print("\n\n")
-            os.system('PAUSE')
-        elif(Numero1 ==3):
-            break
-        else:
-            print("El numero ingresado no se encuentra dentro del rango.")
+                    else:
+                        contador = 1
+                        while contador <= Numero2:
+                            print(f"{contador}",end='')
+                            contador = contador + 1
+                            if (contador<=Numero2):
+                                print(" X ", end='')
+                            else:
+                                print(" = ", math.factorial(Numero2), "\n\n")
+                                os.system('PAUSE')
+                        else:
+                            print(math.factorial(Numero2))
+                except ValueError:
+                    print("El valor ingresado no es un numero\n\n")
+                    os.system('PAUSE')
+            elif(Numero1 == 2):
+                os.system('cls')
+                def fibonacci(n):
+                    fibseq = list([])
+                    a, b = 0, 1
+                    for _ in range(n):
+                        fibseq.append(a)
+                        a, b = b, a + b
+                    print(fibseq) 
+                try:
+                    n = int(input("Ingrese un numero para la secuencia de Fibonacci: "))
+                    if n<0:
+                        print("Ingrese un numero mayor a 0")
+                    else:
+                        secuencia = fibonacci(n)
+                except ValueError:
+                    print("El valor ingresado no es un numero")
+                print("\n\n")
+                os.system('PAUSE')
+            elif(Numero1 ==3):
+                os.system('cls')
+                print("GRACIAS POR USAR EL PROGRAMA")
+                break
+            else:
+                print("\nEl numero ingresado no se encuentra dentro del rango.\n\n")
+                os.system('PAUSE')
+        except ValueError:
+            print("El valor ingresado no es un numero.")
     except ValueError:
-        print("El valor ingresado no es un numero.")
+        os.system('cls')
+        print("El valor ingresado no es un numero.\n\n")
+        os.system('PAUSE')
