abe6744cdd239c6dcd5155f5ed8126cc12c314b5
MarvinCif <144157889+MarvinCif@users.noreply.github.com>
2023-09-28 09:48:46 -0600

    Add files via upload

---
+++ Semana 8/TS8_MECM-1167723.py
@@ -0,0 +1,94 @@
+import tkinter as tk
+from tkinter import *
+
+#Se define una funcion para realizar una secuencia de incremento en 1 con el ciclo for
+def Num1_25for():
+        Numero = []
+        for i in range(1, 26):
+            Numero.append(i)
+        Resultado.config(text=f"Números del 1 al 25 con incremento de 1:\n {Numero}")
+        
+#Se define una funcion para realizar una secuencia de incremento en 1 con el ciclo while
+def Num1_25while():
+    Numero = 0
+    numeros = []
+    while Numero < 25:
+        Numero = Numero+1
+        numeros.append(Numero)
+    Resultado.config(text=f"Números del 1 al 25 con incremento de 1:\n{numeros}")
+
+#Se define una funcion para realizar una secuencia de incremento en 5 con el ciclo for
+def Num5_50for():
+    Numero = []
+    for i in range(5,51,5):
+        Numero.append(i)
+    Resultado.config(text=f"Números del 5 al 50 con incremento de 5:\n {Numero}")
+
+#Se define una funcion para realizar una secuencia de incremento en 5 con el ciclo while
+def Num5_50while():
+    Numero = 0
+    numeros = []
+    while Numero < 50:
+        Numero = Numero+5
+        numeros.append(Numero)
+        Resultado.config(text=f"Numeros del 5 al 50 con incremento de 5:\n  {numeros}")
+
+#Se define una funcion para realizar una secuencia de decremento en 2 con el ciclo for
+def Num20_0for():
+    Numero = []
+    for i in range(20,-1,-2):
+        Numero.append(i)
+    Resultado.config(text=f"Números del 20 al 0 con decremento en 2: {Numero}")
+
+#Se define una funcion para realizar una secuencia de decremento en 2 con el ciclo while
+def Num20_0while():
+    Numero = 22
+    numeros = []
+    while Numero > 0:
+        Numero = Numero-2
+        numeros.append(Numero)
+        Resultado.config(text=f"Numeros del 20 al 0 con decremento en 2:  {numeros}")
+
+#Se define una funcion para realizar la terminacion del programa
+def salida():
+    opcion.destroy()
+
+#Se definen las funciones del programa con TKinter
+
+#Se declara una variable que da inicio a TKinter con el tamaño, letra y separacion de los titulos.
+opcion = tk.Tk()
+opcion.title("TS8 - SECUENCIAS DE NUMEROS")
+opcion.geometry("800x400")
+
+Titulo = tk.Label(opcion, text="TS8 - SECUENCIAS DE NUMEROS", font=("Arial", 14))
+Titulo.pack()
+
+Opciones = tk.Label(opcion, text="\nSeleccione una opción:\n", font=("Arial", 11))
+Opciones.pack()
+
+BotonSecuencia1 = tk.Button(opcion, text="SECUENCIA 1-25 FOR", command=Num1_25for, font=("Arial", 11))
+BotonSecuencia1.pack(pady=4)
+
+BotonSecuencia2 = tk.Button(opcion, text="SECUENCIA 1-25 WHILE", command=Num1_25while, font=("Arial", 11))
+BotonSecuencia2.pack(pady=4)
+
+BotonSecuencia3 = tk.Button(opcion, text="SECUENCIA 1-50 FOR", command=Num5_50for, font=("Arial", 11))
+BotonSecuencia3.pack(pady=4)
+
+BotonSecuencia4 = tk.Button(opcion, text="SECUENCIA 1-50 WHILE", command=Num5_50while, font=("Arial", 11))
+BotonSecuencia4.pack(pady=4)
+
+BotonSecuencia5 = tk.Button(opcion, text="SECUENCIA 20-0 FOR", command=Num20_0for, font=("Arial", 11))
+BotonSecuencia5.pack(pady=4)
+
+BotonSecuencia6 = tk.Button(opcion, text="SECUENCIA 20-0 WHILE", command=Num20_0while, font=("Arial", 11))
+BotonSecuencia6.pack(pady=4)
+
+BotonSalir = tk.Button(opcion, text="Salir", command=salida, font=("Arial", 11))
+BotonSalir.pack(pady=4)
+
+
+Resultado = tk.Label(opcion, text="", font=("Arial", 11))
+Resultado.pack(pady=5)
+
+opcion.mainloop()
